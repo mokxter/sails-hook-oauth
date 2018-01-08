2d9b92e69c05dbffeb323df18e3f019fffaa66a6
mokx <ianbuena081191@gmail.com>
2018-01-08 10:40:43 +0800

    Initial implementation of marlinspike

---
+++ api/hooks/oauth/index.js
@@ -0,0 +1,20 @@
+let oauthHook = sails.hooks.oauth;
+
+import _ from 'lodash';
+import Marlinspike from 'marlinspike';
+
+if (!oauthHook) {
+  class Oauth extends Marlinspike {
+    constructor(sails) {
+      super(sails, module);
+    }
+
+    configure() {
+      sails.services.passport.loadStrategies();
+    }
+  }
+
+  oauthHook = Marlinspike.createSailsHook(Oauth);
+}
+
+export default oauthHook;
---
+++ config/policies.js
@@ -0,0 +1,6 @@
+module.exports.policies = {
+  AuthController: {
+    '*': [ 'passport' ]
+  }
+};
+
